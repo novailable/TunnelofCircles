1af74bf97ced6270395055901656c0540d2bb450
novailable <novailable@icloud.com>
2025-08-05 23:27:49 +0800

    Initial Commit

---
+++ Windows in visionOS/Windows_in_visionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Windows_in_visionOSApp.swift
+//  Windows in visionOS
+//
+//  Created by AUNG TAYZA OO on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Windows_in_visionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
